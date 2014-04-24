7c54c87f3142b0288a657f35175ba2726d79d4df
diegojuniorno <diegojuniorno@hotmail.com>
2014-04-24 20:53:58 -0300

    DIEGOPROJETORODANDO2014

---
+++ README.md
@@ -0,0 +1,7 @@
+Desenvolvedor: Diego Jr. Nascimento Oliveira
+Curso: Sistemas De informacao
+Periodo: 7 Periodo
+Ano: 2014
+Faculdade: UNIFRAN
+Materia: Topicos Avancados Em Sistemas De Informacao I 
+Professor: Edson Lopes
--- projetoFP/settings.py
+++ projetoFP/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for projetoFP project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -17,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+i=oa^$ql9vgvhj4%5l4h1seksbjy$e)%t^yj3dhrq-mqknk-+'
+SECRET_KEY = 'nz4v!uqn!t8g@czh%twe*hoj9e(fi@yii*4uk9iy@pyw8p-mf_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,9 +16,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
